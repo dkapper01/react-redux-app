b0665e9caec90bd006b1516915dc430d62c9fc29
Daniel Kapper <dkapper01@gmail.com>
2018-10-14 18:25:07 -0400

    Added postItems and rendered post id, title, body

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -7,6 +7,7 @@ class Posts extends Component {
       posts: []
     }
   }
+
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(res => res.json())
@@ -14,9 +15,18 @@ class Posts extends Component {
     
   }
   render() {
+    const postItems = this.state.posts.map(post => {
+      return (
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
+      )
+    })
     return (
       <div>
         <h1>Posts</h1>
+        {postItems}
       </div>
     )
   }
